231dbd9e89c3399df88e0c6d8a069e50fd81198d
HyekJung <155180714+HyekJung@users.noreply.github.com>
2024-04-09 00:46:58 +0900

    [level 0] Title: 1로 만들기, Time: 0.01 ms, Memory: 70.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181880. 1로 만들기/1로 만들기.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int solution(int[] num_list) {
+        int cnt = 0;
+        for (int num : num_list) {
+            while (num != 1) {
+                if (num % 2 == 0) {
+                    num /= 2;
+                } else {
+                    num = (num - 1) / 2;
+                }
+                cnt++;
+            }
+        }
+        return cnt;
+    }
+}
